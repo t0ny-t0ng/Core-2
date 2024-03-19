2a0d8275c135b80b52a759fd8988150b744260a6
t0ny-t0ng <untone13@ya.ru>
2024-03-19 16:32:37 +0300

    нужна помощь в классе ордер

---
+++ src/task/collection/Order.java
@@ -0,0 +1,54 @@
+package task.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order {
+// TODO: Учет заказов:
+//  Создайте класс Order с полями id, customer и totalAmount.
+//  Создайте метод, который будет принимать список заказов и выводить заказы с наибольшей суммой.
+//  Создайте метод, который будет принимать список заказов и возвращать общую сумму всех заказов.
+
+    static Integer customerID = 1;
+    static Integer orderID = 1;
+    String customer;
+    Integer totalAmount;
+    String orderData;
+
+    Map<Integer, String> customersMap = new HashMap<>();
+    Map<Integer, String> ordersMap = new HashMap<>();
+    ArrayList<Integer> ordersList = new ArrayList<>();
+
+    public void ordersMaker(String customer, String orderData, Integer totalAmount) {
+        customersMap.put(customerID, this.customer);
+        customerID++;
+        ordersMap.put(orderID, this.orderData);
+        orderID++;
+        ordersList.add(orderID, totalAmount);
+    }
+
+    public void getMaxAmountOrder() {
+        System.out.println(Collections.max(ordersList));
+    }
+
+    public void getOrdersAmountSumm() {
+        Integer summ = 0;
+        for (int i = 0; i < ordersList.size(); i++)
+            summ += ordersList.get(i);
+        System.out.println(summ);
+    }
+
+    public static void main (String[] args) {
+//        ordersMaker("Ivanov", "Tea, Donut", 300);
+//        ordersMaker("Petrov", "Cofee, CheeseCake", 400);
+//        ordersMaker("Sidorov", "Lemonade, Cookie", 250);
+//        ordersMaker("Vasin", "Orange fresh, sandwich", 500);
+//
+//        getMaxAmountOrder();
+//
+//        getOrdersAmountSumm();
+    }
+
+}
